9eeb13eb8bebc2a51cbdcdf5202956f65bdd81b1
Raj Jain <61439792+rajkjain04@users.noreply.github.com>
2023-10-21 21:51:35 +0100

    Time: 6 ms (23.13%), Space: 51.2 MB (8.38%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,39 +1,58 @@
-import java.security.KeyPair;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.LinkedList;
+import java.util.Queue;
 
 class Solution {
     public int numIslands(char[][] grid) {
-        int numberOfIslands = 0;
+        if (grid == null) {
+            return 0;
+        }
+
+        int islands = 0;
         int ROWS = grid.length;
         int COLS = grid[0].length;
-
 
         for (int r = 0; r < ROWS; r++) {
             for (int c = 0; c < COLS; c++) {
                 if (grid[r][c] == '1') {
-                    numberOfIslands += 1;
-                    dfs(grid, r, c);
+                    islands += 1;
+                    bfs(grid, r, c);
                 }
             }
         }
 
-        return numberOfIslands;
+        return islands;
     }
 
-    private void dfs(char[][] grid, int r, int c) {
+    public boolean isValid(int r, int c, char[][] grid) {
         int ROWS = grid.length;
         int COLS = grid[0].length;
 
-        if (r < 0 || r == ROWS || c < 0 || c == COLS || grid[r][c] == '0') {
-            return;
-        }
+        return r >= 0 && r != ROWS && c >= 0 && c != COLS && grid[r][c] != '0';
+    }
 
+    public void bfs(char[][] grid, int r, int c) {
+
+        Queue<int[]> queue = new LinkedList<>();
         grid[r][c] = '0';
 
-        dfs(grid,r + 1, c);
-        dfs(grid, r - 1, c);
-        dfs(grid, r, c + 1);
-        dfs(grid, r, c - 1);
+        queue.add(new int[]{r, c});
+
+        while (!queue.isEmpty()) {
+            int[] cell = queue.poll();
+            int row = cell[0];
+            int col = cell[1];
+
+            int[][] neighbours = new int[][]{new int[]{row + 1, col}, new int[]{row - 1, col}, new int[]{row, col + 1}, new int[]{row, col - 1}};
+
+            for (int[] neighbour: neighbours) {
+                int newRow = neighbour[0];
+                int newCol = neighbour[1];
+
+                if (isValid(newRow, newCol, grid)) {
+                    queue.add(new int[]{newRow, newCol});
+                    grid[newRow][newCol] = '0';
+                }
+            }
+        }
     }
 }
