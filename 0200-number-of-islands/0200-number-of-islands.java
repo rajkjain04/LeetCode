fd19e0531d9e6cd8e2cb43e84f29e0c0df7368aa
Raj Jain <61439792+rajkjain04@users.noreply.github.com>
2023-10-21 20:30:32 +0100

    Time: 9 ms (10.80%), Space: 50.7 MB (11.04%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -0,0 +1,39 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public int numIslands(char[][] grid) {
+        int numberOfIslands = 0;
+        int ROWS = grid.length;
+        int COLS = grid[0].length;
+
+
+        for (int r = 0; r < ROWS; r++) {
+            for (int c = 0; c < COLS; c++) {
+                if ("1".compareTo(String.valueOf(grid[r][c])) == 0) {
+                    numberOfIslands += 1;
+                    dfs(grid, r, c);
+                }
+            }
+        }
+
+        return numberOfIslands;
+    }
+
+    private void dfs(char[][] grid, int r, int c) {
+        int ROWS = grid.length;
+        int COLS = grid[0].length;
+
+        if (r < 0 || r == ROWS || c < 0 || c == COLS || "0".compareTo(String.valueOf(grid[r][c])) == 0) {
+            return;
+        }
+        
+        grid[r][c] = '0';
+        
+
+        dfs(grid,r + 1, c);
+        dfs(grid, r - 1, c);
+        dfs(grid, r, c + 1);
+        dfs(grid, r, c - 1);
+    }
+}
