bddfa330236d2dcde19a9140971f76ffb2f0255c
Raj Jain <61439792+rajkjain04@users.noreply.github.com>
2023-10-26 22:27:10 +0100

    Time: 317 ms (35.16%), Space: 23.9 MB (39.80%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,39 +1,46 @@
 class Solution:
-    def numIslands(self, grid: List[List[str]]) -> int:
+    def numIslands(self, grid: List[List[str]]) -> int: 
         
-        if not grid: 
+        if not grid:
             return 0 
         
+        islands = 0 
         ROWS, COLS = len(grid), len(grid[0])
         visit = set() 
-        islands = 0 
         
         def isValid(r, c):
             
-            if (r < 0 or r == ROWS or c < 0 or c == COLS) or (grid[r][c] == "0" or (r, c) in visit):
-                return False
-            
+            if (r < 0 or r == ROWS or c < 0 or c == COLS) or (r,c) in visit or grid[r][c] == "0":
+                return False 
+                
             return True 
             
-        
-        def bfs(r, c): 
-            queue = [] 
+        def bfs(r, c):
             visit.add((r, c))
-            queue.append((r, c))
+            queue = [] 
             
+            queue.append((r, c))
+                   
             while queue:
-                row, col = queue.pop(0) 
-                neighbours = [(row - 1, col), (row + 1, col), (row, col + 1), (row, col- 1)]
-                
+                r, c = queue.pop(0)
+                neighbours = [(r + 1, c), (r - 1, c), (r, c + 1), (r, c - 1)]
+
                 for nx, ny in neighbours:
                     if isValid(nx, ny):
-                        queue.append((nx, ny))
                         visit.add((nx, ny))
+                        queue.append((nx, ny))
                 
-        for r in range(0, ROWS):
-            for c in range(0, COLS):
-                if grid[r][c] == "1" and (r, c) not in visit:
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == "1" and (r,c) not in visit:
                     islands += 1 
                     bfs(r, c)
-        
+                    
         return islands 
+        
+            
+        
+        
+        
+        
+        
