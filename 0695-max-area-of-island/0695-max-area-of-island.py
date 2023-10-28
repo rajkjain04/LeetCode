3445eaaf0d4ea3bd4982e6902e17df960d124a11
Raj Jain <61439792+rajkjain04@users.noreply.github.com>
2023-10-28 15:43:15 +0100

    Time: 127 ms (62.24%), Space: 16.8 MB (77.89%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -1,34 +1,45 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
         
+        if not grid:
+            return 0 
+        
         ROWS, COLS = len(grid), len(grid[0])
-        visit = set() 
-        total = 0 
-        area = 0
+        visit = set()
+        max_area = 0 
+        
+        def isValid(r,c):
+            if (r < 0 or r == ROWS or c < 0 or c == COLS) or (r, c) in visit or grid[r][c] == 0:
+                return False 
+            
+            return True 
         
         def dfs(r, c):
-            # Base case 
-            if (r < 0 or r == ROWS or c < 0 or c == COLS) or (grid[r][c] == 0 or (r,c) in visit):
-                return 0 
+            area = 0
+            stack = [(r, c)] 
             
-            visit.add((r,c))
+            while stack: 
+                r, c = stack.pop() 
+                
+                neighbours = [(r - 1, c), (r + 1, c),  (r, c - 1), (r, c + 1)]
+                
+                for nx, ny in neighbours:
+                    if isValid(nx, ny):
+                        visit.add((nx, ny))
+                        area += 1 
+                        stack.append((nx, ny))
             
-            down = dfs(r + 1, c)
-            up = dfs(r - 1, c)
-            right = dfs(r, c + 1)
-            left = dfs(r, c - 1)
+            return area 
             
-            return 1 + down + up + right + left 
-        
         for r in range(ROWS):
             for c in range(COLS):
-                area = max(area, dfs(r, c))
-                
-        return area 
-            
-            
-        
-        
-        
-        
+                if grid[r][c] == 1 and (r, c) not in visit:
+                    visit.add((r, c))
+                    area = 1 + dfs(r, c) 
+                    max_area = max(area, max_area)
+                    
+        return max_area 
+                    
+                    
+                    
         
