461f907cb63bca7a7c6a689152bb2834caed6fbc
Raj Jain <61439792+rajkjain04@users.noreply.github.com>
2023-10-29 15:01:10 +0000

    Time: 61 ms (80.87%), Space: 17.3 MB (57.72%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,17 +1,25 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         
+        hash_map = {} 
         output = [] 
-        storage = {} 
         
-        for i in range(0, len(nums)): 
-            if nums[i] not in storage:
-                storage[target-nums[i]] = i
-
-            else: 
+        for i in range(len(nums)):
+            value = target - nums[i] 
+            
+            if value in hash_map:
+                output.append(hash_map[value])
                 output.append(i)
-                output.append(storage[nums[i]])
+                
                 return output 
+            
+            else: 
+                hash_map[nums[i]] = i 
+                
                 
         
-        return output 
+                
+                
+            
+        
+        
