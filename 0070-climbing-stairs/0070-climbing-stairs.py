7b40d3772f54cc9126b7d5afd461bebc11a3c240
Raj Jain <61439792+rajkjain04@users.noreply.github.com>
2023-10-30 21:06:09 +0000

    Time: 43 ms (18.94%), Space: 16.3 MB (39.88%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,32 +1,21 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
         
+        if n <= 1:
+            return n 
         
-        cache = {} 
+        dp = [1,1]
         
-        # Using Recursion        
-        def dfs(i, n, cache):
-            
-            if i > n:
-                return 0 
-            
-            if i == n:
-                return 1 
-            
-            if i in cache:
-                return cache[i]
-            
-            left = dfs(i + 1, n, cache)
-            right = dfs(i + 2, n, cache)
-            
-            cache[i] = left + right 
-            
-            return cache[i]
+        i = 2 
         
-        return dfs(0, n, cache)
+        while i <= n:
+            tmp = dp[1]
+            dp[1] = dp[1] + dp[0] 
+            dp[0] = tmp 
+            i += 1 
             
-            
-            
+        return dp[1]
+        
         
         
         
