a15e2447e2be97ca483f6fe18e6bc83aceb69b75
Raj Jain <61439792+rajkjain04@users.noreply.github.com>
2023-11-04 20:40:13 +0000

    Time: 126 ms (51.32%), Space: 18.9 MB (54.28%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,17 +1,9 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         
-        seen = set() 
+        a = 0 
         
-        for i in range(0, len(nums)):
-            if nums[i] not in seen:
-                seen.add(nums[i]) 
+        for i in nums:
+            a ^= i 
             
-            else:
-                seen.remove(nums[i])
-                
-        for item in seen:
-            return item 
-            
-        
-        
+        return a 
