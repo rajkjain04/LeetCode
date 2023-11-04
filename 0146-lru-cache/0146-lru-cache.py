0186cb0a2c5c063c7bd1ac4bb5152391f010bb56
Raj Jain <61439792+rajkjain04@users.noreply.github.com>
2023-11-04 14:22:09 +0000

    Time: 688 ms (62.48%), Space: 78.4 MB (33.29%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,52 +1,57 @@
-class Node:
+class Node: 
     def __init__(self, key, val):
         self.key = key 
-        self.val = val 
-        self.next = None 
+        self.val = val
         self.prev = None 
+        self.next = None 
 
 class LRUCache:
     def __init__(self, capacity: int):
         self.cap = capacity 
         self.cache = {} 
-        self.left = Node(0, 0)
-        self.right = Node(0, 0)
+        self.left = Node(0,0)
+        self.right = Node(0,0)
         self.left.next = self.right 
-        self.right.prev = self.left
-        
-    def insert(self, node):
-        tmp = self.right.prev 
-        tmp.next = node 
-        self.right.prev = node 
-        node.prev = tmp 
-        node.next = self.right 
-        
-    def remove(self, node):
-        prev, nxt = node.prev, node.next 
-        prev.next = nxt 
-        nxt.prev = prev 
+        self.right.prev = self.left 
     
-    def get(self, key: int) -> int:
-        if key in self.cache:
-            self.remove(self.cache[key])
-            self.insert(self.cache[key])
-            return self.cache[key].val
+    # Removes the provided node 
+    def remove(self, node):
+        left = node.prev 
+        right = node.next 
+        left.next = right 
+        right.prev = left 
         
+    # Inserts the node at the end of the list (before the right node) 
+    def insert(self, node):
+        prevNode = self.right.prev
+        self.right.prev = node 
+        node.next = self.right
+        node.prev = prevNode
+        prevNode.next = node 
+    
+    
+    def get(self, key: int) -> int:    
+        if key in self.cache: 
+            node = self.cache[key]
+            self.remove(node)
+            self.insert(node)
+            return node.val 
+            
         return -1 
         
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
-            self.remove(self.cache[key])
-        
-        self.cache[key] = Node(key, value)
-        self.insert(self.cache[key])
+            node = self.cache[key]
+            self.remove(node)
+         
+        node = Node(key, value)
+        self.cache[key] = node 
+        self.insert(node)
         
         if len(self.cache) > self.cap:
-            # Remove from the list and delete the LRU 
             lru = self.left.next 
             self.remove(lru)
             del self.cache[lru.key]
-            
         
 
 # Your LRUCache object will be instantiated and called as such:
