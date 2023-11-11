1a69a0287f8b3a3cc5a993992cb54e6463c283e9
Raj Jain <61439792+rajkjain04@users.noreply.github.com>
2023-11-11 15:29:49 +0000

    Time: 230 ms (45.83%), Space: 26.5 MB (70.03%) - LeetHub

--- 1396-design-underground-system/1396-design-underground-system.py
+++ 1396-design-underground-system/1396-design-underground-system.py
@@ -2,12 +2,10 @@ class UndergroundSystem:
 
     def __init__(self):
         self.averageTimes = {} 
-        self.totalJourneys = {}
         self.checkInMap = {} 
         
 
     def checkIn(self, id: int, stationName: str, t: int) -> None:
-        
         self.checkInMap[id] = (stationName, t)
         
     def checkOut(self, id: int, stationName: str, t: int) -> None:
@@ -16,22 +14,21 @@ class UndergroundSystem:
         totalTime = t - timeIn
         
         if (srcName, stationName) not in self.averageTimes:
-            self.averageTimes[(srcName, stationName)] = totalTime 
-            self.totalJourneys[(srcName, stationName)] = 1 
+            self.averageTimes[(srcName, stationName)] = (totalTime, 1)
              
         else:
-            currentTime = self.averageTimes[(srcName, stationName)]
-            totalJourneys = self.totalJourneys[(srcName, stationName)]
+            currentTime, totalJourneys = self.averageTimes[(srcName, stationName)]
             totalPrevTime = currentTime * totalJourneys
             averageTime = (totalPrevTime + totalTime) / (totalJourneys + 1)
             
-            self.averageTimes[(srcName, stationName)] = averageTime 
-            self.totalJourneys[(srcName, stationName)] += 1 
+            self.averageTimes[(srcName, stationName)] = (averageTime, totalJourneys + 1) 
+            
+        self.checkInMap.pop(id)
         
 
     def getAverageTime(self, startStation: str, endStation: str) -> float:
         
-        return self.averageTimes[(startStation, endStation)]
+        return self.averageTimes[(startStation, endStation)][0]
 
 # Your UndergroundSystem object will be instantiated and called as such:
 # obj = UndergroundSystem()
