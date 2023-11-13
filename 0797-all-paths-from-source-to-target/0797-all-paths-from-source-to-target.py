8fe8d88a1d67ae4a3bc03ebe96a12dcb5efccdc3
Raj Jain <61439792+rajkjain04@users.noreply.github.com>
2023-11-13 10:31:44 +0000

    Time: 92 ms (23.42%), Space: 18.3 MB (39.56%) - LeetHub

---
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
@@ -0,0 +1,27 @@
+class Solution:
+    def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+        
+        res = [] 
+        self.graph = {} 
+        
+        for i, edge in enumerate(graph):
+            self.graph[i] = edge
+            
+        def dfs(curr_path, curr_node): 
+            if (curr_node) == len(self.graph) - 1:
+                res.append(list(curr_path))
+                
+            for connection in self.graph[curr_node]:
+                curr_path.append(connection)
+                dfs(curr_path, connection) 
+                curr_path.pop() 
+                
+        dfs([0], 0)
+        
+        return res 
+            
+            
+            
+            
+            
+        
