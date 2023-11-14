1d84da80638065f28ecccc6847fc2eaf001c46e2
Raj Jain <61439792+rajkjain04@users.noreply.github.com>
2023-11-14 12:30:31 +0000

    Time: 3 ms (12.19%), Space: 40.8 MB (15.44%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,37 +1,46 @@
 class Solution {
     public boolean isValid(String s) {
 
-        HashSet<Character> set = new HashSet<>();
-        set.add('(');
-        set.add('[');
-        set.add('{');
+
+        if (s.length() == 0) {
+            return false;
+        }
 
         Stack<Character> stack = new Stack<>();
 
-        for (int i = 0; i < s.length(); i++) {
+        HashSet<Character> hashSet = new HashSet<>();
+        hashSet.add('[');
+        hashSet.add('(');
+        hashSet.add('{');
 
-            if (set.contains(s.charAt(i))) {
-                stack.add(s.charAt(i));
-            } else {
+        for (int i = 0; i < s.length(); i++) {
+            Character character = s.charAt(i);
+
+            if (hashSet.contains(character)) {
+                stack.add(character);
+            }
+
+            else {
 
                 if (stack.isEmpty()) {
                     return false;
                 }
 
                 Character lastElement = stack.lastElement();
-                char currentElement = s.charAt(i);
-
-                if ((lastElement == '(' && currentElement == ')') || (lastElement == '[' && currentElement == ']') || (lastElement == '{' && currentElement == '}')) {
+                if ((lastElement.equals('[') && character.equals(']')) || (lastElement.equals('(') && character.equals(')')) || (lastElement.equals('{') && character.equals('}'))) {
                     stack.pop();
                 }
-
                 else {
                     return false;
                 }
-
             }
         }
+
         return stack.isEmpty();
 
+
+
+
+ 
     }
 }
