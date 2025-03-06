53fe0f7a56da9e95df679dd270333c4bca0d9762
Raj Jain <61439792+rajkjain04@users.noreply.github.com>
2025-03-06 15:40:52 +0000

    Time: 147 ms (85.24%) | Memory: 19 MB (49.50%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -0,0 +1,24 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+
+        # Need to determine the speed k 
+
+        leftPointer = 1 
+        rightPointer = max(piles)
+        minSpeed = float('inf')
+
+        while leftPointer <= rightPointer: 
+            middlePointer = (leftPointer + rightPointer) // 2 
+
+            hours = 0 
+            for pile in piles:
+                hours += math.ceil(pile / middlePointer)
+
+            if hours > h: 
+                leftPointer = middlePointer + 1 
+            
+            else: 
+                minSpeed = min(minSpeed, middlePointer)
+                rightPointer = middlePointer - 1 
+        
+        return minSpeed 
